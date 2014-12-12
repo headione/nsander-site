b959554a9b4eca8d336c57599ffa6cf2ecd83bf3
Norman Sander <norman@nsander.de>
2014-12-12 16:40:43 +0100

    grunt setup

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,30 +1,48 @@
 module.exports = function(grunt) {
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    jshint: {
-      files: ['Gruntfile.js', 'js/**/*.js'],
-      options: {
-        globals: {
-          jQuery: true
-        }
-      }
-    },
-    watch: {
-      scripts: {
-        files: ['**/*.js'],
-        tasks: ['jshint'],
-        options: {
-          spawn: false,
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            dev: {
+                options: {
+                    style: 'expanded'
+                },
+                files: {
+                    'css/style.css': 'sass/style.scss'
+                }
+            },
+
+            dist: {
+                options: {
+                    banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n',
+                    style: 'compressed'
+                },
+                files: {
+                    'css/style.css': 'sass/style.scss'
+                }
+            }
         },
-      },
-    },
+        watch: {
+            css: {
+                files: 'sass/*.scss',
+                tasks: ['sass:dev'],
+                options: {
+                    livereload: true,
+                },
+            },
+            scripts: {
+                files: ['**/*.js'],
+                tasks: ['jshint'],
+                options: {
+                    spawn: false,
+                },
+            },
+        },
 
-  });
+    });
 
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-
-  grunt.registerTask('default', ['jshint']);
-
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('dist', ['sass:dist']);
 };
